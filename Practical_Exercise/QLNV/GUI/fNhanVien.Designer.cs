5c3af2dfbdb1c3ac5a1893cbb32a7e82a713c723
VinhKy01 <info.vinhky@gmail.com>
2022-03-07 22:08:42 +0700

    Update Exercise

--- Practical_Exercise/QLNV/GUI/fNhanVien.Designer.cs
+++ Practical_Exercise/QLNV/GUI/fNhanVien.Designer.cs
@@ -29,33 +29,167 @@
         private void InitializeComponent()
         {
             this.dtgvNhanVien = new System.Windows.Forms.DataGridView();
+            this.txtManv = new System.Windows.Forms.TextBox();
+            this.txtHolot = new System.Windows.Forms.TextBox();
+            this.txtTenNhanVien = new System.Windows.Forms.TextBox();
+            this.txtPhai = new System.Windows.Forms.TextBox();
+            this.cbbMacv = new System.Windows.Forms.ComboBox();
+            this.lblManv = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpkNgaySinh = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvNhanVien)).BeginInit();
             this.SuspendLayout();
             // 
             // dtgvNhanVien
             // 
             this.dtgvNhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgvNhanVien.Location = new System.Drawing.Point(12, -1);
+            this.dtgvNhanVien.Location = new System.Drawing.Point(12, 201);
             this.dtgvNhanVien.Name = "dtgvNhanVien";
-            this.dtgvNhanVien.Size = new System.Drawing.Size(730, 394);
+            this.dtgvNhanVien.Size = new System.Drawing.Size(730, 192);
             this.dtgvNhanVien.TabIndex = 0;
+            // 
+            // txtManv
+            // 
+            this.txtManv.Location = new System.Drawing.Point(145, 56);
+            this.txtManv.Name = "txtManv";
+            this.txtManv.Size = new System.Drawing.Size(164, 20);
+            this.txtManv.TabIndex = 1;
+            // 
+            // txtHolot
+            // 
+            this.txtHolot.Location = new System.Drawing.Point(145, 93);
+            this.txtHolot.Name = "txtHolot";
+            this.txtHolot.Size = new System.Drawing.Size(164, 20);
+            this.txtHolot.TabIndex = 1;
+            // 
+            // txtTenNhanVien
+            // 
+            this.txtTenNhanVien.Location = new System.Drawing.Point(145, 134);
+            this.txtTenNhanVien.Name = "txtTenNhanVien";
+            this.txtTenNhanVien.Size = new System.Drawing.Size(164, 20);
+            this.txtTenNhanVien.TabIndex = 1;
+            // 
+            // txtPhai
+            // 
+            this.txtPhai.Location = new System.Drawing.Point(444, 93);
+            this.txtPhai.Name = "txtPhai";
+            this.txtPhai.Size = new System.Drawing.Size(164, 20);
+            this.txtPhai.TabIndex = 1;
+            // 
+            // cbbMacv
+            // 
+            this.cbbMacv.FormattingEnabled = true;
+            this.cbbMacv.Location = new System.Drawing.Point(444, 56);
+            this.cbbMacv.Name = "cbbMacv";
+            this.cbbMacv.Size = new System.Drawing.Size(164, 21);
+            this.cbbMacv.TabIndex = 2;
+            // 
+            // lblManv
+            // 
+            this.lblManv.AutoSize = true;
+            this.lblManv.Location = new System.Drawing.Point(64, 59);
+            this.lblManv.Name = "lblManv";
+            this.lblManv.Size = new System.Drawing.Size(75, 13);
+            this.lblManv.TabIndex = 3;
+            this.lblManv.Text = "Ma Nhan Vien";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ho Lot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ten Nhan Vien";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(363, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ma Cong Viec";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(363, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phai";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(363, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "\'Ngay Sinh";
+            // 
+            // dtpkNgaySinh
+            // 
+            this.dtpkNgaySinh.Location = new System.Drawing.Point(444, 134);
+            this.dtpkNgaySinh.Name = "dtpkNgaySinh";
+            this.dtpkNgaySinh.Size = new System.Drawing.Size(164, 20);
+            this.dtpkNgaySinh.TabIndex = 4;
             // 
             // fNhanVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(745, 405);
+            this.Controls.Add(this.dtpkNgaySinh);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblManv);
+            this.Controls.Add(this.cbbMacv);
+            this.Controls.Add(this.txtTenNhanVien);
+            this.Controls.Add(this.txtPhai);
+            this.Controls.Add(this.txtHolot);
+            this.Controls.Add(this.txtManv);
             this.Controls.Add(this.dtgvNhanVien);
             this.Name = "fNhanVien";
             this.Text = "fNhanVien";
             this.Load += new System.EventHandler(this.fNhanVien_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtgvNhanVien)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dtgvNhanVien;
+        private System.Windows.Forms.TextBox txtManv;
+        private System.Windows.Forms.TextBox txtHolot;
+        private System.Windows.Forms.TextBox txtTenNhanVien;
+        private System.Windows.Forms.TextBox txtPhai;
+        private System.Windows.Forms.ComboBox cbbMacv;
+        private System.Windows.Forms.Label lblManv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpkNgaySinh;
     }
 }
